a78f9d2bffabe9de5c9eed02e30d0a93c23c2d46
srk112006 <65439988+srk112006@users.noreply.github.com>
2020-05-23 20:12:18 +0530

    Add files via upload

---
+++ imyth.py
@@ -0,0 +1,50 @@
+print ("This is the Indian Mythology Quiz!! Get ready!")
+print ("Type your answers as 'A','B','C' or 'D'")
+print ("Who poisoned Bhima during their younger years? ")
+ans = input("Your options are A-Dushasana, B-Shakuni,C-Vikarna,D- Duryodhana ")
+if ans == "D":
+    print ("Good job! The questions will get harder!")
+else:
+    print ("Sorry! This answer is wrong! The right answer was D-Duryodhana")
+
+    
+print ("How many childeren did Sita give birth to? ")
+ans = input("Your options are A-1, B-2,C-3,D- None ")
+if ans == "A":
+    print ("Good job! The questions will get harder!")
+else:
+    print ("Sorry! This answer is wrong! The right answer was A-1")
+
+    
+print ("Who is Chandra's favorite wife? ")
+ans = input("Your options are A-Krittika, B-Rohini,C-Ashvini,D- Swati ")
+if ans == "B":
+    print ("Good job! The questions will get harder!")
+else:
+    print ("Sorry! This answer is wrong! The right answer was B-Rohini")
+
+    
+print ("Who was the stone woman in the ramayana? ")
+ans = input("Your options are A-Ahalya, B-Vimala,C-Asha,D- Dushalla ")
+if ans == "A":
+    print ("Good job! The questions will get harder!")
+else:
+    print ("Sorry! This answer is wrong! The right answer was A-Ahalya")
+
+print ("Who was the grandson of Krishna? ")
+ans = input("Your options are A-Paurava, B-Yadu,C-Aniruddha,D- Vasudeva ")
+if ans == "C":
+    print ("Good job! The quiz is over!")
+else:
+    print ("Sorry! This answer is wrong! The right answer was C-Aniruddha. The quiz is over!")
+
+
+
+
+
+
+
+
+
+
+
